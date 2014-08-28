a3b3829a866a754ab139298d8449cee096f476fe
André Wendt <andre.wendt@infopark.de>
2014-08-28 10:21:41 +0200

    Refactor the game code into board and gameplay/levels

---
+++ board.js
@@ -0,0 +1,108 @@
+function Board(canvas) {
+  this.canvas = canvas;
+  this.circles = [];
+  this.text_nodes = [];
+  this.handlers = {};
+}
+
+Board.prototype.empty = function() {
+  while(this.circles.length > 0) {
+    this.circles.pop().remove();
+  }
+  while(this.text_nodes.length > 0) {
+    this.text_nodes.pop().remove();
+  }
+};
+
+Board.prototype.draw = function(num_circles) {
+  this.empty();
+
+  var free_boxes = [{x: 0, y: 0, width: this.canvas.width(), height: this.canvas.height()}];
+  var radius = 50;
+
+  var calculate_free_boxes_within = function(box, draw_at) {
+    var boxes = [];
+    if (draw_at.y-box.y > radius) {
+      boxes.push({x: box.x, y: box.y, width: draw_at.x+radius-box.x, height: draw_at.y-box.y});
+    }
+    if (draw_at.x-box.x > radius) {
+      boxes.push({x: box.x, y: draw_at.y, width: draw_at.x-box.x, height: box.y+box.height-draw_at.y});
+    }
+    if (box.x+box.width-draw_at.x > 2*radius) {
+      boxes.push({x: draw_at.x+radius, y: box.y, width: box.x+box.width-draw_at.x-radius, height: draw_at.y+radius-box.y});
+    }
+    if (box.x+box.width-draw_at.x > radius && (box.y+box.height-draw_at.y-radius) > radius) {
+      boxes.push({x: draw_at.x, y: draw_at.y+radius, width: box.x+box.width-draw_at.x, height: box.y+box.height-draw_at.y-radius});
+    }
+    return boxes;
+  };
+
+  var replace_current_box = function(index, new_boxes) {
+    Array.prototype.splice.apply(free_boxes, [index, 1].concat(new_boxes));
+  };
+
+  var random_position = function() {
+    var index = Math.floor(Math.random() * free_boxes.length);
+    var box = free_boxes[index];
+    var draw_at = {
+      x: Math.floor(Math.random()*(box.width-radius)) + box.x,
+      y: Math.floor(Math.random()*(box.height-radius)) + box.y
+    };
+    replace_current_box(index, calculate_free_boxes_within(box, draw_at));
+    return {x: draw_at.x, y: draw_at.y};
+  };
+  var targets = [];
+  while (targets.length < num_circles) {
+    targets.push(random_position());
+  }
+  var index;
+  for (index=0; index < targets.length; ++index) {
+    var group = canvas.group();
+    var x = targets[index].x;
+    var y = targets[index].y;
+    this.circles.push(group
+      .circle(radius)
+      .stroke({color: '#69B6D1', width: '5'})
+      .fill({color: '#E6DBCA'})
+      .move(x, y)
+      .data('index', index)
+    );
+    this.text_nodes.push(group
+      .plain((index+1).toString())
+      .font({family: 'Arial, Helvetica, sans-serif', size: 30})
+      .center(x+(radius/2), y+(radius/2))
+      .fill({color: '#69B6D1'})
+      .data('index', index)
+    );
+  }
+
+  var check_click = function(board) {
+    return function() {
+      if (board.circles.length === num_circles) {
+        // remove all text_nodes if this is the first move
+        for (index=0; index < board.text_nodes.length; ++index) {
+          board.text_nodes[index].remove();
+        }
+      }
+      var circle = board.circles.shift();
+      circle.remove();
+      if (circle.data('index') !== this.data('index')) {
+        // remove all circles if user didn't touch the correct one
+        board.empty();
+        board.handlers.fail();
+      }
+      else if (board.circles.length === 0) {
+        board.handlers.done();
+      }
+    }
+  }
+
+  this.text_nodes[0].on('click', check_click(this));
+  for (index=0; index < this.text_nodes.length; ++index) {
+    this.circles[index].on('click', check_click(this));
+  }
+};
+
+Board.prototype.on = function(input, fn) {
+  this.handlers[input] = fn;
+};
--- game.js
+++ game.js
@@ -1,90 +1,19 @@
-(function() {
-  var draw = SVG('canvas').size(window.innerWidth, window.innerHeight);
-  var free_boxes = [{x: 0, y: 0, width: draw.width(), height: draw.height()}];
-  var radius = 50;
-  var num_circles = 6;
+// http://www.colourlovers.com/palette/3459622/Flowering_Tiles
 
-  var calculate_free_boxes_within = function(box, draw_at) {
-    var boxes = [];
-    if (draw_at.y-box.y > radius) {
-      boxes.push({x: box.x, y: box.y, width: draw_at.x+radius-box.x, height: draw_at.y-box.y});
-    }
-    if (draw_at.x-box.x > radius) {
-      boxes.push({x: box.x, y: draw_at.y, width: draw_at.x-box.x, height: box.y+box.height-draw_at.y});
-    }
-    if (box.x+box.width-draw_at.x > 2*radius) {
-      boxes.push({x: draw_at.x+radius, y: box.y, width: box.x+box.width-draw_at.x-radius, height: draw_at.y+radius-box.y});
-    }
-    if (box.x+box.width-draw_at.x > radius && (box.y+box.height-draw_at.y-radius) > radius) {
-      boxes.push({x: draw_at.x, y: draw_at.y+radius, width: box.x+box.width-draw_at.x, height: box.y+box.height-draw_at.y-radius});
-    }
-    return boxes;
-  };
+var canvas = SVG('canvas').size(window.innerWidth, window.innerHeight);
+var board = new Board(canvas);
+var level = 4;
+board.draw(level);
+board.on('done', function() {
+  level += 1;
+  board.draw(level);
+});
+board.on('fail', function() {
+  alert("YOU LOSE!");
+});
 
-  var replace_current_box = function(index, new_boxes) {
-    Array.prototype.splice.apply(free_boxes, [index, 1].concat(new_boxes));
-  };
-
-  var random_position = function() {
-    var index = Math.floor(Math.random() * free_boxes.length);
-    var box = free_boxes[index];
-    var draw_at = {
-      x: Math.floor(Math.random()*(box.width-radius)) + box.x,
-      y: Math.floor(Math.random()*(box.height-radius)) + box.y
-    };
-    replace_current_box(index, calculate_free_boxes_within(box, draw_at));
-    return {x: draw_at.x, y: draw_at.y};
-  };
-  var targets = [];
-  while (targets.length < num_circles) {
-    targets.push(random_position());
-  }
-  var circles = [];
-  var text_nodes = [];
-  var index;
-  for (index=0; index < targets.length; ++index) {
-    var group = draw.group();
-    var x = targets[index].x;
-    var y = targets[index].y;
-    circles.push(group
-      .circle(radius)
-      .stroke({color: '#69B6D1', width: '5'})
-      .fill({color: '#E6DBCA'})
-      .move(x, y)
-      .data('index', index)
-    );
-    text_nodes.push(group
-      .plain((index+1).toString())
-      .font({family: 'Arial, Helvetica, sans-serif', size: 30})
-      .center(x+(radius/2), y+(radius/2))
-      .fill({color: '#69B6D1'})
-      .data('index', index)
-    );
-  }
-  var check_click = function() {
-    if (circles.length === num_circles) {
-      // remove all text_nodes if this is the first move
-      for (index=0; index < text_nodes.length; ++index) {
-        text_nodes[index].remove();
-      }
-    }
-    var circle = circles.shift();
-    circle.remove();
-    if (circle.data('index') !== this.data('index')) {
-      // remove all circles if user didn't touch the correct one
-      while(circles.length > 0) {
-        circles.pop().remove();
-      }
-      alert("YOU LOSE!");
-    }
-    // if (circles.length === 0) {
-    //   LEVEL UP!
-    // }
-  }
-
-  text_nodes[0].on('click', check_click);
-  for (index=0; index < text_nodes.length; ++index) {
-    circles[index].on('click', check_click);
-  }
-
-}());
+// Responsibilities:
+// 1. Drawing
+// 2. User input (correct move, wrong move)
+// 3. Gameplay (levels, notifications)
+// 4. Stopwatch
