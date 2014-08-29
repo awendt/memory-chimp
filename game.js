e93a3ffaaf37988ed2b49a09f5ac916801fa0bb1
André Wendt <andre.wendt@infopark.de>
2014-08-29 13:55:28 +0200

    Level indicators: Bananas

--- game.js
+++ game.js
@@ -5,6 +5,8 @@ var board = new Board(canvas);
 var level = 4;
 board.draw(level);
 board.on('done', function() {
+  var level_banana = document.getElementById("level"+level);
+  level_banana.className = level_banana.className + " done";
   level += 1;
   board.draw(level);
 });
