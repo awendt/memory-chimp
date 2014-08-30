9a689d52ddc4dc8ed131f346fdce4a32908913a8
André Wendt <awendt@freeshell.org>
2014-08-30 22:56:08 +0200

    Bigger instructions

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 // http://www.colourlovers.com/palette/3459622/Flowering_Tiles
 
-var canvas = SVG('canvas').size(window.innerWidth, window.innerHeight-109);
+var canvas = SVG('canvas').size(window.innerWidth, window.innerHeight-113);
 var board = new Board(canvas);
 var level = 4;
 board.draw(level);
