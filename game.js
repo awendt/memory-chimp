0490d23443cebde3c4c2b80f1562e161a54bb2d8
André Wendt <awendt@freeshell.org>
2014-09-12 21:29:52 +0200

    Announce that player has won when cracking 9 numbers

--- game.js
+++ game.js
@@ -1,9 +1,15 @@
 // http://www.colourlovers.com/palette/3459622/Flowering_Tiles
 
+var $elements = {
+  game_over: document.getElementById('game_over'),
+  you_won: document.getElementById('you_won'),
+};
 var canvas = SVG('canvas').size(window.innerWidth-40, window.innerHeight-document.getElementById('canvas').offsetTop-20);
 var board = new Board(canvas);
 var stopwatch = new StopWatch({delay: 5, timer: document.getElementById('instructions')});
 var level = 4;
+var MAX_LEVEL = 9;
+
 board.draw(level);
 board.on('start', function() {
   stopwatch.reset();
@@ -11,27 +17,28 @@ board.on('start', function() {
 });
 board.on('done', function() {
   stopwatch.stop();
-  var level_banana = document.getElementById("level"+level);
-  level_banana.className = level_banana.className + " done";
-  level += 1;
-  setTimeout(function() {
-    board.draw(level);
-  }, 500);
+  document.getElementById("level"+level).classList.add("done");
+  if (level == MAX_LEVEL) {
+    $elements.you_won.classList.add("active");
+  } else {
+    level += 1;
+    setTimeout(function() {
+      board.draw(level);
+    }, 500);
+  }
 });
 board.on('fail', function() {
   stopwatch.stop();
-  document.getElementById('game_over').className = 'active';
+  $elements.game_over.classList.add("active");
+});
+
+document.getElementById('start_over').addEventListener('click', function() {
+  location.reload();
 });
 
 document.getElementById('retry').addEventListener('click', function() {
-  document.getElementById('game_over').className = '';
+  $elements.game_over.classList.remove("active");
   board.draw(level);
 });
 
 document.body.addEventListener('touchmove', function(e){ e.preventDefault(); });
-
-// Responsibilities:
-// 1. Drawing
-// 2. User input (correct move, wrong move)
-// 3. Gameplay (levels, notifications)
-// 4. Stopwatch
